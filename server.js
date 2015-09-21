ec5cdf00167035276aeec25226c18801e5be262a
mguenther006 <mguenther006@users.noreply.github.com>
2015-09-21 16:03:13 -0600

    removed port prefix

--- server.js
+++ server.js
@@ -37,39 +37,39 @@ app.put('/api/address/:id', AddressCtrl.update);
 // app.delete('/api/address/:id', AddressCtrl.delete);
 
 // app.post('/api/daycare', DaycareCtrl.create);
-app.get(':80/api/daycare', DaycareCtrl.read);
-app.put(':80/api/daycare/:id', DaycareCtrl.update);
+app.get('/api/daycare', DaycareCtrl.read);
+app.put('/api/daycare/:id', DaycareCtrl.update);
 // app.delete('/api/daycare/:id', DaycareCtrl.delete);
 
 // app.post('/api/grooming', GroomingCtrl.create);
-app.get(':80/api/grooming', GroomingCtrl.read);
-app.put(':80/api/grooming/:id', GroomingCtrl.update);
+app.get('/api/grooming', GroomingCtrl.read);
+app.put('/api/grooming/:id', GroomingCtrl.update);
 // app.delete('/api/grooming/:id', GroomingCtrl.delete);
 
-app.post(':80/api/product', ProductCtrl.create);
-app.get(':80/api/product', ProductCtrl.read);
-app.put(':80/api/product/:id', ProductCtrl.update);
-app.delete(':80/api/product/:id', ProductCtrl.delete);
+app.post('/api/product', ProductCtrl.create);
+app.get('/api/product', ProductCtrl.read);
+app.put('/api/product/:id', ProductCtrl.update);
+app.delete('/api/product/:id', ProductCtrl.delete);
 
-app.post(':80/api/rating', RatingCtrl.create);
-app.get(':80/api/rating', RatingCtrl.read);
-app.put(':80/api/rating/:id', RatingCtrl.update);
-app.delete(':80/api/rating/:id', RatingCtrl.delete);
+app.post('/api/rating', RatingCtrl.create);
+app.get('/api/rating', RatingCtrl.read);
+app.put('/api/rating/:id', RatingCtrl.update);
+app.delete('/api/rating/:id', RatingCtrl.delete);
 
-app.post(':80/api/spa', SpaCtrl.create);
-app.get(':80/api/spa', SpaCtrl.read);
-app.put(':80/api/spa/:id', SpaCtrl.update);
-app.delete(':80/api/spa/:id', SpaCtrl.delete);
+app.post('/api/spa', SpaCtrl.create);
+app.get('/api/spa', SpaCtrl.read);
+app.put('/api/spa/:id', SpaCtrl.update);
+app.delete('/api/spa/:id', SpaCtrl.delete);
 
-app.post(':80/api/daycareRates', DaycareRatesCtrl.create);
-app.get(':80/api/daycareRates', DaycareRatesCtrl.read);
-app.put(':80/api/daycareRates/:id', DaycareRatesCtrl.update);
-app.delete(':80/api/daycareRates/:id', DaycareRatesCtrl.delete);
+app.post('/api/daycareRates', DaycareRatesCtrl.create);
+app.get('/api/daycareRates', DaycareRatesCtrl.read);
+app.put('/api/daycareRates/:id', DaycareRatesCtrl.update);
+app.delete('/api/daycareRates/:id', DaycareRatesCtrl.delete);
 
-app.post(':80/api/users', UserCtrl.create);
-app.get(':80/api/users', UserCtrl.read);
-app.put(':80/api/users/:id', UserCtrl.update);
-app.delete(':80/api/users/:id', UserCtrl.delete);
+app.post('/api/users', UserCtrl.create);
+app.get('/api/users', UserCtrl.read);
+app.put('/api/users/:id', UserCtrl.update);
+app.delete('/api/users/:id', UserCtrl.delete);
 
 // app.post('/api/orders', OrderCtrl.create);
 // app.get('/api/orders', OrderCtrl.read);
